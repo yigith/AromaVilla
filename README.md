ea4bba6f5439516abf9f4cfd8341cb23227848cb
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2023-03-29 11:09:30 +0300

    dbcontext, migrations

--- README.md
+++ README.md
@@ -13,6 +13,13 @@ Install-Package Ardalis.Specification -v 6.1.0
 Install-Package Microsoft.EntityFrameworkCore -v 6.0.15
 Install-Package Microsoft.EntityFrameworkCore.Tools -v 6.0.15
 Install-Package Npgsql.EntityFrameworkCore.PostgreSQL -v 6.0.8
+Install-Package Microsoft.AspNetCore.Identity.EntityFrameworkCore -v 6.0.15
+Install-Package Microsoft.EntityFrameworkCore.Design -v 6.0.15
+```
+
+### Web
+```
+Install-Package Microsoft.EntityFrameworkCore.Tools -v 6.0.15
 ```
 
 ## Migrations
@@ -20,6 +27,9 @@ Before running the following commands, make sure that Web is set as startup proj
 
 ### Infrastructure
 ```
-Add-Migration InitialCreate -context ShopContext -OutputDir Data/Migrations
-Update-Database -context ShopContext
+Add-Migration InitialCreate -Context ShopContext -OutputDir Data/Migrations
+Update-Database -Context ShopContext
+
+Add-Migration InitialIdentity -Context AppIdentityDbContext -OutputDir Identity/Migrations
+Update-Database -Context AppIdentityDbContext
 ```
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,20 +1,20 @@
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ShopContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ShopContext")));
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AppIdentityDbContext")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppIdentityDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
