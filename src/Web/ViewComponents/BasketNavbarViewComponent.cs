056e6322d292d36ed99bc003e0f2459ebac04015
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2023-04-04 12:18:29 +0300

    basket navbar view component

---
+++ src/Web/ViewComponents/BasketNavbarViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.ViewComponents
+{
+    public class BasketNavbarViewComponent : ViewComponent
+    {
+        private readonly IBasketViewModelService _basketViewModelService;
+
+        public BasketNavbarViewComponent(IBasketViewModelService basketViewModelService)
+        {
+            _basketViewModelService = basketViewModelService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _basketViewModelService.GetBasketViewModelAsync());
+        }
+    }
+}
